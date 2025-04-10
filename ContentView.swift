ff62f9666c0d4a20464e07752ad3092c10cfaf10
saraalcauter <salcauter8013@stu.d214.org>
2025-04-10 10:46:37 -0500

    added textfield

--- ContentView.swift
+++ ContentView.swift
@@ -4,12 +4,7 @@ struct ContentView: View {
     @State var number1: Int = 0
     @State var number2 = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-           
-        }
+        
+        TextField("#", value: $number1, format: .number)
     }
 }
